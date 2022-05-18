b3f18997d5a46c59378a7a565a8a2f9c992567bc
camrynalexx <camryn.cam@gmail.com>
2022-05-17 23:50:29 -0700

    added a new component, a background, and a footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Dictionary />
+      <footer className="text-center">
+        Coded by
+        <a href="https://www.linkedin.com/in/camrynalexx/"> Camryn Lee </a>
+        and open-sourced on <a href="https://github.com/camrynalexx/dictionary-app.git"> Github </a>
+        and hosted on <a href="https://shiny-bubblegum-be753f.netlify.app"> netlify </a>
+      </footer>
     </div>
   );
 }
